f68961d657e281982fe4309520e4be92f8fe67ef
Temmi556 <temmi556@mail.ru>
2025-10-07 13:07:59 +0300

    Исправление Homework_3.c

--- Homework_3.c
+++ Homework_3.c
@@ -4,7 +4,8 @@ int main() {
     float current_price,procent,plus_price;
     procent = 1.25;
     plus_price = 12.50;
+    printf("Ввод значения: ");
     scanf("%f",&current_price);
     printf("При увеличении цены %.1f на 25 процентов стоимость будет = %.1f\n",current_price,current_price*procent);
-    printf("При увеличении цены %.1f на 12.5 стоимость будет = %.1f ", current_price,current_price+plus_price);
+    printf("При увеличении цены %.1f на 12.5 стоимость будет = %.1f\n", current_price,current_price+plus_price);
 }
